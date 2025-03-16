73e0faecbabe99cd41833c2ea87b6913dff393e1
Habib-human <habibulislam.mist@gmail.com>
2025-03-16 14:12:25 -0400

    Fix import of main function in __init__.py

--- plasann/__init__.py
+++ plasann/__init__.py
@@ -5,3 +5,5 @@ PlasAnn - A tool for plasmid annotation and visualization
 __version__ = '1.0.5'
 __author__ = 'Habibul Islam'
 __email__ = 'hislam2@ur.rochester.edu'
+
+from .annotate_plasmid import main
